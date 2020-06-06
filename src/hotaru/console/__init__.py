bdb9fc408084f076f876ad167e8204172f7faddc
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-06 16:36:58 +0900

    depress error message

--- src/hotaru/console/__init__.py
+++ src/hotaru/console/__init__.py
@@ -1,7 +1,16 @@
-from .application import Application
-
-
 def main():
     import os
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    import tensorflow as tf
+    tf.get_logger().setLevel('ERROR')
+
+    devs = tf.config.list_physical_devices('GPU')
+    for d in devs:
+        tf.config.experimental.set_memory_growth(d, True)
+
+    #tf.config.experimental_run_functions_eagerly(True)
+    #tf.autograph.set_verbosity(1, True)
+
+    from .application import Application
     return Application().run()

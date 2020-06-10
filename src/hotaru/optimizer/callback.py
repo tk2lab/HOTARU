3dcef55f8d3f8baf6f2983b7409fe1990453d626
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-10 23:23:07 +0900

    Update: replace varaiables on epoch end

--- src/hotaru/optimizer/callback.py
+++ src/hotaru/optimizer/callback.py
@@ -1,7 +1,14 @@
 import tensorflow as tf
+import tensorflow.keras.backend as K
 
 
 class Callback(tf.keras.callbacks.Callback):
 
     def on_epoch_begin(self, epoch, logs=None):
         self.model.optimizer.start = self.model.optimizer.iterations
+
+    def on_epoch_end(self, epoch, logs=None):
+        vs = self.model.trainable_weights
+        for v in vs:
+            o = self.model.optimizer.get_slot(v, 'old')
+            K.set_value(v, K.get_value(o))

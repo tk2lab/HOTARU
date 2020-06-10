681b27e227465cdde17fa9ada78edcd8888c091b
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-10 21:52:41 +0900

    Update: reverse mask file

--- src/hotaru/data/mask.py
+++ src/hotaru/data/mask.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 import numpy as np
 import tifffile
 
+from ..util.gs import ensure_local_file
+
 
 def get_mask(masktype, h, w, job_dir='.'):
     if masktype[-4:] == '.pad':
@@ -11,12 +13,19 @@ def get_mask(masktype, h, w, job_dir='.'):
         mask = np.zeros([h, w], bool)
         mask[pad:h-pad,pad:w-pad] = True
     else:
-        with tf.io.gfile.GFile(os.path.join(job_dir, masktype), 'rb') as fp:
-            if masktype[-4:] == '.tif':
-                mask = tifffile.imread(fp)
-            elif masktype[-4:] == '.npy':
-                mask = np.load(fp)
+        if masktype[:2] == 'r:':
+            maskfile = ensure_local_file(os.path.join(job_dir, masktype[2:]))
+        else:
+            maskfile = ensure_local_file(os.path.join(job_dir, masktype))
+        if masktype[-4:] == '.tif':
+            mask = tifffile.imread(maskfile)
+        elif masktype[-4:] == '.npy':
+            mask = np.load(maskfile)
+        else:
+            raise RuntimeErorr('bad file type: {maskfile}')
         mask = mask > 0
+        if masktype[:2] == 'r:':
+            mask = ~mask
     return mask
 
 

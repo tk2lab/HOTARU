60815aed4d787571b8e8ee343febc41fa49b40eb
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-11 09:24:10 +0900

    Refactoring: separate functions

--- src/hotaru/train/callback.py
+++ src/hotaru/train/callback.py
@@ -4,47 +4,70 @@ import tensorflow.keras.backend as K
 from matplotlib import cm
 
 
+_jet = cm.get_cmap('jet')
+_reds = cm.get_cmap('Reds')
+_greens = cm.get_cmap('Greens')
+
+
+def normalized_and_sort(val):
+    mag = val.max(axis=1)
+    idx = np.argsort(mag)[::-1]
+    val /= mag[:, None]
+    val = val[idx]
+    return val, mag
+
+
+def calc_cor(val):
+    cor = val @ val.T
+    scale = np.sqrt(np.diag(cor))
+    cor /= scale * scale[:, None]
+    return cor
+
+
+def spike_summary(val, step=0):
+    val, mag = normalized_and_sort(val)
+    cor = calc_cor(val)
+    tf.summary.histogram('magnitude/spike', mag, step=step)
+    tf.summary.image('spike-val', _reds(val)[None, ...], step=step)
+    tf.summary.image('cor/spike', _jet(cor)[None, ...], step=step)
+
+
+def footprint_summary(val, mask, mag=None, name='footprint', step=0):
+    if mag is None:
+        val, mag = normalized_and_sort(val)
+    else:
+        idx = np.argsort(mag)[::-1]
+        val = val[idx]
+    cor = calc_cor(val)
+
+    nk, nx = val.shape
+    h, w = mask.shape
+    imgs = np.zeros((nk, h, w))
+    imgs[:, mask] = val
+
+    imgs_max = imgs.max(axis=0)
+    tf.summary.histogram(f'area/{name}', val.sum(axis=1), step=step)
+    tf.summary.image(f'cor/{name}', _jet(cor)[None, ...], step=step)
+    tf.summary.image(f'max/{name}', _greens(imgs_max)[None, ...], step=step)
+    if name == 'footprint':
+        tf.summary.histogram(f'magnitude/{name}', mag, step=step)
+        tf.summary.image(f'{name}-val', _greens(imgs[[0,1,-2,-1]]), max_outputs=4, step=step)
+    else:
+        tf.summary.histogram(name, mag, step=step)
+        for i, img in enumerate(imgs):
+            tf.summary.image(f'{name}-val', _greens(img[None, ...]), step=i)
+
+
 class HotaruCallback(tf.keras.callbacks.TensorBoard):
 
     def on_epoch_end(self, epoch, logs=None):
         super().on_epoch_end(epoch, logs)
 
-        jet = cm.get_cmap('jet')
-        reds = cm.get_cmap('Reds')
-        greens = cm.get_cmap('Greens')
         mode = K.get_value(self.model.variance._mode)
         writer = self._get_writer(self._train_run_name)
         with writer.as_default():
             if mode == 0:
-                val = self.model.spike.val
-                m = val.max(axis=1)
-                tf.summary.histogram('spike/magnitude', m, step=epoch)
-                mag = val.max(axis=1)
-                idx = np.argsort(mag)[::-1]
-                val /= mag[:, None]
-                val = val[idx]
-                cor = val @ val.T
-                scale = np.sqrt(np.diag(cor))
-                cor /= scale * scale[:, None]
-                tf.summary.image('spike-val', reds(val)[None, ...], step=epoch)
-                tf.summary.image('cor/spike', jet(cor)[None, ...], step=epoch)
+                spike_summary(self.model.spike.val, step=epoch)
             else:
-                val = self.model.footprint.val
-                mag = val.max(axis=1)
-                idx = np.argsort(mag)[::-1]
-                val /= mag[:, None]
-                val = val[idx]
-                cor = val @ val.T
-                scale = np.sqrt(np.diag(cor))
-                cor /= scale * scale[:, None]
                 mask = self.model.mask
-                nk, nx = val.shape
-                h, w = mask.shape
-                imgs = np.zeros((nk, h, w))
-                imgs[:, mask] = val
-                tf.summary.histogram('footprint/magnitude', mag, step=epoch)
-                tf.summary.histogram('footprint/area', val.sum(axis=1), step=epoch)
-                tf.summary.image('cor/footprint', jet(cor)[None, ...], step=epoch)
-                tf.summary.image('footprint-max', greens(imgs.max(axis=0, keepdims=True)), step=epoch)
-                tf.summary.image('footprint-val/0', greens(imgs[:3]), step=epoch)
-                tf.summary.image('footprint-val/1', greens(imgs[-3:]), step=epoch)
+                footprint_summary(self.model.footprint.val, mask, step=epoch)

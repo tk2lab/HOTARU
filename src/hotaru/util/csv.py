13220e2e2b2e502866c6ec72450bd069a64b4fc7
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-13 23:34:36 +0900

    Fix

--- src/hotaru/util/csv.py
+++ src/hotaru/util/csv.py
@@ -1,8 +1,16 @@
 import tensorflow as tf
 import pandas as pd
+import numpy as np
 
 
 def save_csv(base, peak, name):
     peak_dict = {k: v for k, v in zip(name, peak)}
-    with tf.io.gfile.GFile(base + '.csv', 'w') as fp:
+    with tf.io.gfile.GFile(f'{base}.csv', 'w') as fp:
         pd.DataFrame(peak_dict).to_csv(fp)
+
+
+def load_csv(base, name, typ):
+    with tf.io.gfile.GFile(f'{base}.csv', 'r') as fp:
+        peak = pd.read_csv(fp)
+        peak = tuple(np.array(peak[k], t) for k, t in zip(name, typ))
+    return peak

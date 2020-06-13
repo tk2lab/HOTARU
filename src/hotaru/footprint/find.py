9bf4e764753d6b551fee06c97a6f8567000bf928
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-13 10:45:04 +0900

    Fix for return value order of gaussian_laplace_multi.

--- src/hotaru/footprint/find.py
+++ src/hotaru/footprint/find.py
@@ -1,10 +1,10 @@
+import tensorflow.keras.backend as K
 import tensorflow as tf
-import numpy as np
 
 from ..util.distribute import distributed, ReduceOp
-from ..data.dataset import unmasked
-from ..image.gaussian import gaussian
-from ..image.laplace import gaussian_laplace_multi
+from ..util.dataset import unmasked
+from ..image.filter.gaussian import gaussian
+from ..image.filter.laplace import gaussian_laplace_multi
 
 
 def find_peak(data, mask, gauss, radius, thr_gl, batch, prog=None):
@@ -18,21 +18,21 @@ def find_peak(data, mask, gauss, radius, thr_gl, batch, prog=None):
         max_gl = tf.nn.max_pool3d(
             gl[..., None], [1, 3, 3, 3, 1], [1, 1, 1, 1, 1], padding='SAME'
         )[..., 0]
-        bit = tf.equal(gl, max_gl) & (gl > thr_gl) & mask[..., None]
+        bit = tf.equal(gl, max_gl) & (gl > thr_gl) & mask
         posr = tf.cast(tf.where(bit), tf.int32)
         ts = tf.gather(times, posr[:, 0])
-        ys = posr[:, 1]
-        xs = posr[:, 2]
-        rs = tf.gather(radius, posr[:, 3])
+        rs = tf.gather(radius, posr[:, 1])
+        ys = posr[:, 2]
+        xs = posr[:, 3]
         gs = tf.gather_nd(gl, posr)
         return ts, rs, ys, xs, gs
 
     data = unmasked(data, mask)
     data = data.enumerate().batch(batch)
-    mask = tf.convert_to_tensor(mask)
-    gauss = tf.convert_to_tensor(gauss)
-    radius = tf.convert_to_tensor(radius)
-    thr_gl = tf.convert_to_tensor(thr_gl)
+    mask = K.constant(mask, tf.bool)
+    gauss = K.constant(gauss)
+    radius = K.constant(radius)
+    thr_gl = K.constant(thr_gl)
     peak = _find(data, mask, gauss, radius, thr_gl, prog=prog)
     idx = tf.argsort(peak[-1])[::-1]
     return tuple(tf.gather(v, idx).numpy() for v in peak)

ff7597bc39f92d6108cf8185eeff25ec5c64e843
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-13 10:44:45 +0900

    Simplify

--- src/hotaru/footprint/make.py
+++ src/hotaru/footprint/make.py
@@ -1,16 +1,14 @@
-import time
-
 import tensorflow as tf
 
-from ..data.dataset import unmasked
-from ..image.gaussian import gaussian
-from ..image.laplace import gaussian_laplace_multi
 from ..util.distribute import distributed, ReduceOp
+from ..util.dataset import unmasked
+from ..image.filter.gaussian import gaussian
+from ..image.filter.laplace import gaussian_laplace_multi
 from .segment import get_segment_index
 from .util import get_normalized_val, get_magnitude, ToDense
 
 
-def make_footprint(dataset, mask, gauss, radius, ts, rs, ys, xs, batch):
+def make_footprint(dataset, mask, gauss, radius, peaks, batch):
     strategy = tf.distribute.get_strategy()
 
     mask = tf.convert_to_tensor(mask, tf.bool)
@@ -20,6 +18,8 @@ def make_footprint(dataset, mask, gauss, radius, ts, rs, ys, xs, batch):
 
     gauss = tf.convert_to_tensor(gauss, tf.float32)
     radius = tf.convert_to_tensor(radius, tf.float32)
+
+    ts, rs, ys, xs = peaks[:4]
     ts = tf.convert_to_tensor(ts, tf.int32)
     rs = tf.convert_to_tensor(rs, tf.int32)
     ys = tf.convert_to_tensor(ys, tf.int32)
@@ -27,25 +27,21 @@ def make_footprint(dataset, mask, gauss, radius, ts, rs, ys, xs, batch):
 
     prog = tf.keras.utils.Progbar(tf.size(ts).numpy())
     fps = tf.TensorArray(tf.float32, 0, True)
-    mgs = tf.TensorArray(tf.float32, 0, True)
     e = tf.constant(0)
     for imgs in strategy.experimental_distribute_dataset(dataset):
         s, e = e, e + tf.shape(imgs)[0]
-        gls, tl, rl, yl, xl = _prepare(
+        gs, yl, xl = _prepare(
             imgs, s, e, ts, rs, ys, xs, gauss, radius,
         )
-        nk = tf.size(tl)
+        nk = tf.size(yl)
         for k in tf.range(nk):
-            t, r, y, x = tl[k], rl[k], yl[k], xl[k]
-            gl = gls[t, :, :, r]
+            g, y, x = gl[k], yl[k], xl[k]
             pos = get_segment_index(gl, y, x, mask)
             val = get_normalized_val(gl, pos)
-            mag = get_magnitude(gl, pos)
             footprint = to_dense(pos, val)
-            mgs = mgs.write(mgs.size(), mag)
             fps = fps.write(fps.size(), footprint)
             prog.add(1)
-    return fps.stack(), mgs.concat()
+    return fps.stack().numpy()
 
 
 @distributed(*[ReduceOp.CONCAT for _ in range(5)], loop=False)
@@ -59,4 +55,5 @@ def _prepare(imgs, start, end, ts, rs, ys, xs, gauss, radius):
     if gauss > 0.0:
         imgs = gaussian(imgs, gauss)
     gls = gaussian_laplace_multi(imgs, radius)
-    return gls, tl, rl, yl, xl
+    gl = tf.gather_nd(gls, tf.stack([tl, rl], 1))
+    return gl, yl, xl

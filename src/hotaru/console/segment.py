d973aae1473beb7eec2541f6d62bb469075b92a3
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-15 08:42:08 +0900

    Log before/after reduce

--- src/hotaru/console/segment.py
+++ src/hotaru/console/segment.py
@@ -31,38 +31,57 @@ class SegmentCommand(Command):
 
     def create(self, key, stage):
         self.line('segment')
+
         gauss, rmin, rmax, rnum, thr_gl, shard = key[-2][1:]
-        radius = tuple(0.001 * round(1000 * x) for x in np.linspace(rmin, rmax, rnum))
+        radius = tuple(
+            0.001 * round(1000 * x) for x in np.linspace(rmin, rmax, rnum)
+        )
+        inv = {v: i for i, v in enumerate(radius)}
         thr_dist = key[-1][1]
         batch = self.status['root']['batch']
-        peak = reduce_peak(self.peak, thr_dist)
-        ts, rs, ys, xs, gs = peak
-        cond = (radius[0] < rs) & (rs < radius[-1])
-        ts, rs, ys, xs, gs = map(lambda s: s[cond], (ts, rs, ys, xs, gs))
-        inv = {v: i for i, v in enumerate(radius)}
-        rs_id = np.array([inv[0.001 * round(1000 * r)] for r in rs], np.int32)
-        peak_id = ts, rs_id, ys, xs, gs
-        data = self.data.shard(shard, 0)
-        mask = self.mask
-        footprint = make_footprint(data, mask, gauss, radius, peak_id, batch)
 
-        mask = self.mask
         log_dir = os.path.join(
             self.application.job_dir, 'logs', 'segment',
             datetime.now().strftime('%Y%m%d-%H%M%S'),
         )
         writer = tf.summary.create_file_writer(log_dir)
         with writer.as_default():
+            # all peak
+            ts, rs, ys, xs, gs = self.peak
+            #tf.summary.histogram(f'radius/{stage:03d}', rs, step=0)
+            #tf.summary.histogram(f'laplacian/{stage:03d}', gs, step=0)
+
+            # remove edge
+            cond = (radius[0] < rs) & (rs < radius[-1])
+            ts, rs, ys, xs, gs = map(lambda s: s[cond], (ts, rs, ys, xs, gs))
+            #tf.summary.histogram(f'radius/{stage:03d}', rs, step=1)
+            #tf.summary.histogram(f'laplacian/{stage:03d}', gs, step=1)
+
+            # reduce for multiple thr_dist
+            n = 10
+            peak = ts, rs, ys, xs, gs
+            for i in range(6, n + 1):
+                thr = i * thr_dist / n
+                ts, rs, ys, xs, gs = reduce_peak(peak, thr)
+                tf.summary.histogram(f'radius/{stage:03d}', rs, step=i)
+                tf.summary.histogram(f'laplacian/{stage:03d}', gs, step=i)
+            writer.flush()
+
+            # make footprint
+            data = self.data.shard(shard, 0)
+            mask = self.mask
+            rs_id = np.array([inv[0.001 * round(1000 * r)] for r in rs], np.int32)
+            peak_id = ts, rs_id, ys, xs, gs
+            footprint = make_footprint(
+                data, mask, gauss, radius, peak_id, batch,
+            )
             fsum = footprint.sum(axis=1)
             tf.summary.histogram(f'size/{stage:03d}', fsum, step=0)
-            tf.summary.histogram(f'radius/{stage:03d}', rs, step=0)
-            tf.summary.histogram(f'laplacian/{stage:03d}', gs, step=0)
             summary_footprint_stat(footprint, mask, stage)
             summary_footprint(footprint, mask, stage)
         writer.close()
 
-        peak = ts, rs, ys, xs, gs
-        return footprint, peak
+        return footprint, (ts, rs, ys, xs, gs)
 
     def save(self, base, val):
         footprint, peak = val

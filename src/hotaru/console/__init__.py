fdd8284a45db904dab0acdbd3881e22dadd1a552
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-15 08:37:54 +0900

    Make GPU memory limit option.

--- src/hotaru/console/__init__.py
+++ src/hotaru/console/__init__.py
@@ -2,6 +2,8 @@ def main():
     import os
     import tensorflow as tf
 
+    limit_gpu_memory = None
+
     # for debug
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
     tf.get_logger().setLevel('WARN')
@@ -9,16 +11,17 @@ def main():
     # tf.config.experimental_run_functions_eagerly(True)
     #tf.autograph.set_verbosity(1, True)
 
-    devs = tf.config.list_physical_devices('GPU')
-    for d in devs:
-        tf.config.experimental.set_memory_growth(d, True)
-
     # limit GPU memory
-    tf.config.set_logical_device_configuration(
-        devs[0], [
-            tf.config.LogicalDeviceConfiguration(memory_limit=1024*7),
-        ],
-    )
+    devs = tf.config.list_physical_devices('GPU')
+    if limit_gpu_memory is not None:
+        tf.config.set_logical_device_configuration(
+            devs[0], [
+                tf.config.LogicalDeviceConfiguration(memory_limit=1024*7),
+            ],
+        )
+    else:
+        for d in devs:
+            tf.config.experimental.set_memory_growth(d, True)
 
     from .application import Application
     return Application().run()

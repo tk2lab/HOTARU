9181b5cfe46fb2016e19a2d28a92ffad7aeb8e07
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-14 16:54:52 +0900

    Simplify

--- src/hotaru/console/__init__.py
+++ src/hotaru/console/__init__.py
@@ -1,18 +1,19 @@
 def main():
     import os
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
     import tensorflow as tf
-    tf.get_logger().setLevel('ERROR')
 
     # for debug
-    tf.debugging.set_log_device_placement(True)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    tf.get_logger().setLevel('WARN')
+    #tf.debugging.set_log_device_placement(True)
     # tf.config.experimental_run_functions_eagerly(True)
     #tf.autograph.set_verbosity(1, True)
 
     devs = tf.config.list_physical_devices('GPU')
-    # for d in devs:
-    #    tf.config.experimental.set_memory_growth(d, True)
+    for d in devs:
+        tf.config.experimental.set_memory_growth(d, True)
+
+    # limit GPU memory
     tf.config.set_logical_device_configuration(
         devs[0], [
             tf.config.LogicalDeviceConfiguration(memory_limit=1024*7),

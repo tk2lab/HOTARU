3f935739635aa3d6c4e6068b7acd33225cd9120a
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-17 23:21:37 +0900

    Convert to Tensor for safety

--- src/hotaru/util/dataset.py
+++ src/hotaru/util/dataset.py
@@ -1,3 +1,4 @@
+import tensorflow.keras.backend as K
 import tensorflow as tf
 
 
@@ -7,15 +8,19 @@ def normalized(data, avgt, avgx, std):
         return (img - avgt - avgx) / std
 
     avgt = tf.data.Dataset.from_tensor_slices(avgt)
+    avgx = K.constant(avgx)
+    std = K.constant(std)
     zip_data = tf.data.Dataset.zip((data, avgt))
     return zip_data.map(normalize)
 
 
 def masked(data, mask):
+    mask = K.constant(mask, tf.bool)
     return data.map(lambda x: tf.boolean_mask(x, mask))
 
 
 def unmasked(data, mask):
+    mask = K.constant(mask, tf.bool)
     rmap = tf.cast(tf.where(mask), tf.int32)
     shape = tf.shape(mask)
     return data.map(lambda x: tf.scatter_nd(rmap, x, shape))

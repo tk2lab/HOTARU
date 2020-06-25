d4b0a359ebdf9470877ff5daf79c5bc1677d43ef
TAKEKAWA Takashi <takekawa@tk2lab.org>
2020-06-25 23:10:14 +0900

    Remove dependency pandas

--- src/hotaru/util/csv.py
+++ src/hotaru/util/csv.py
@@ -1,16 +1,12 @@
 import tensorflow as tf
-import pandas as pd
 import numpy as np
 
 
-def save_csv(base, peak, name):
-    peak_dict = {k: v for k, v in zip(name, peak)}
-    with tf.io.gfile.GFile(f'{base}.csv', 'w') as fp:
-        pd.DataFrame(peak_dict).to_csv(fp)
-
-
-def load_csv(base, name, typ):
+def load_csv(base):
     with tf.io.gfile.GFile(f'{base}.csv', 'r') as fp:
-        peak = pd.read_csv(fp)
-        peak = tuple(np.array(peak[k], t) for k, t in zip(name, typ))
-    return peak
+        return np.loadtxt(fp, delimiter=',', skiprows=1)
+
+
+def save_csv(base, data, fmt, header):
+    with tf.io.gfile.GFile(f'{base}.csv', 'w') as fp:
+        np.savetxt(fp, data, fmt, ',', header=header)

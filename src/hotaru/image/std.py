dac0d74d9e8419ac381870fe23a143c5a7eade80
TAKEKAWA Takashi <takekawa@tk2lab.org>
2022-03-27 08:17:32 +0000

    Add calculation of min and max

--- src/hotaru/image/std.py
+++ src/hotaru/image/std.py
@@ -7,22 +7,24 @@ from ..util.distribute import distributed, ReduceOp
 
 def calc_std(data, mask, nt=None, verbose=1):
 
-    @distributed(ReduceOp.CONCAT, ReduceOp.SUM, ReduceOp.SUM, ReduceOp.SUM)
+    @distributed(ReduceOp.MIN, ReduceOp.MAX, ReduceOp.CONCAT, ReduceOp.SUM, ReduceOp.SUM, ReduceOp.SUM)
     def _calc(imgs, mask):
         masked = tf.boolean_mask(imgs, mask, axis=1)
+        min_tx = K.min(masked)
+        max_tx = K.max(masked)
         avg_t = K.mean(masked, axis=1)
         sum_x = K.sum(imgs, axis=0)
         sumsq = K.sum(K.square(masked - avg_t[:, None]))
         nt = K.cast_to_floatx(K.shape(imgs)[0])
-        return avg_t, sum_x, sumsq, nt
+        return min_tx, max_tx, avg_t, sum_x, sumsq, nt
 
     mask = K.constant(mask, tf.bool)
     nx = K.cast_to_floatx(tf.math.count_nonzero(mask))
     with trange(nt, desc='Calc Stats', disable=verbose == 0) as prog:
-        avg_t, sum_x, sumsq, nt = _calc(data, mask, prog=prog)
+        min_t, max_t, avg_t, sum_x, sumsq, nt = _calc(data, mask, prog=prog)
     avg_x = sum_x / nt
     avg_0 = K.mean(avg_t)
-    avg_t -= avg_0
-    avgxm = tf.boolean_mask(avg_x - avg_0, mask)
+    avg_x -= avg_0
+    avgxm = tf.boolean_mask(avg_x, mask)
     std = K.sqrt(sumsq / nt / nx - K.mean(K.square(avgxm)))
-    return std.numpy(), avg_t.numpy(), avg_x.numpy()
+    return min_t.numpy(), max_t.numpy(), std.numpy(), avg_t.numpy(), avg_x.numpy()

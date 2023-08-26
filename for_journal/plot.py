4da7027c1b66d586eaeb30cfe01dc06112fa8a42
TAKEKAWA Takashi <takekawa@tk2lab.org>
2023-08-27 08:18:03 +0900

    Tune

--- for_journal/plot.py
+++ for_journal/plot.py
@@ -2,32 +2,36 @@ from pathlib import Path
 
 import hydra
 
-from hotaru.cui.common import all_stats, get_data
-
-from hotaru.plot.seg import seg_max_image, bg_sum_image, segs_image, seg_max_fig
-from hotaru.plot.spike import spike_image
-
+from hotaru.cui.common import (
+    all_stats,
+    get_data,
+)
 from hotaru.plot.num import cell_num_fig
-from hotaru.plot.spike import spike_stats_fig
-from hotaru.plot.seg import footprint_stats_fig
 from hotaru.plot.run import run_fig
+from hotaru.plot.seg import (
+    bg_sum_image,
+    footprint_stats_fig,
+    seg_max_fig,
+    segs_image,
+)
+from hotaru.plot.spike import spike_image
 
 
 @hydra.main(version_base=None, config_path="pkg://hotaru.conf", config_name="config")
 def main(cfg):
-
     name = cfg.data.label
 
     final = len(all_stats(cfg)) - 1
+    # final = 15
     h, w = get_data(cfg).shape
 
     fig_dir = Path("figs")
     fig_dir.mkdir(parents=True, exist_ok=True)
 
-    """
     footprint_stats_fig(cfg, [0], usefind=True, width=200, height=200).write_image(
         fig_dir / f"{name}0peak.pdf",
     )
+    """
     seg_max_image(cfg, 0, base=0.5).save(
         fig_dir / f"{name}0segmax.png",
         dpi=(w / 3, w / 3),
@@ -58,9 +62,41 @@ def main(cfg):
     cell_num_fig(cfg, width=400, height=200).write_image(
         fig_dir / f"{name}run_num.pdf",
     )
-    run_fig(cfg, [1, final], thr_f=-1, thr_d=1, min_val=(0, 0), max_val=(0.58, 0.4), min_r=3).write_image(
+    run_fig(
+        cfg,
+        [1, final],
+        thr_f=cfg.clean.args.thr_bg_firmness,
+        thr_d=cfg.clean.args.thr_bg_udense,
+        min_val=(0.2, 0),
+        max_val=(0.58, 0.2),
+        min_r=1,
+        max_r=3,
+    ).write_image(
         fig_dir / f"{name}run.pdf",
     )
+
+    segs_image(cfg, final, mx=10, hsize=25, pad=2).save(
+        fig_dir / f"{name}finalseg.png",
+        dpi=(255, 255),
+    )
+    seg_max_fig(cfg, final, base=0.2, width=300).write_image(
+        fig_dir / f"{name}finalsegmax.pdf",
+    )
+    bg_sum_image(cfg, final).save(
+        fig_dir / f"{name}finalbgsum.png",
+        dpi=(w / 3, w / 3),
+    )
+
+    img, nt, nk = spike_image(
+        cfg,
+        final,
+        tsel=slice(t0 := 400, t1 := 800),
+    )
+    img.save(
+        fig_dir / f"{name}finalspike.png",
+        dpi=(int(0.3 * nt), int(0.3 * nk)),
+    )
+
     """
     spike_stats_fig(cfg, [0, 1, final], width=600, height=200).write_image(
         fig_dir / f"{name}run_spike.pdf",
@@ -68,32 +104,11 @@ def main(cfg):
     footprint_stats_fig(cfg, [0, 1, final], width=600, height=200).write_image(
         fig_dir / f"{name}run_footprint.pdf",
     )
-    """
-
-    segs_image(cfg, final, mx=10, hsize=25, pad=2).save(
-        fig_dir / f"{name}finalseg.png",
-        dpi=(255, 255),
-    )
-    """
     seg_max_image(cfg, final, base=0.0).save(
         fig_dir / f"{name}finalsegmax.png",
         dpi=(w / 3, w / 3),
     )
     """
-    seg_max_fig(cfg, final, base=0.2).write_image(
-        fig_dir / f"{name}finalsegmax.pdf",
-    )
-    bg_sum_image(cfg, final).save(
-        fig_dir / f"{name}finalbgsum.png",
-        dpi=(w / 3, w / 3),
-    )
-    img, nt, nk = spike_image(
-        cfg, final, tsel=slice(t0:=400, t1:=800),
-    )
-    img.save(
-        fig_dir / f"{name}finalspike.png",
-        dpi=(int(0.3 * nt), int(0.3 * nk)),
-    )
 
 
 main()

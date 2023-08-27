11c05b14eec9654ef40b2486d5b100f4fd8086b6
TAKEKAWA Takashi <takekawa@tk2lab.org>
2023-08-27 23:27:25 +0900

    Add multi plot of dense-sig

--- src/hotaru/plot/densesig.py
+++ src/hotaru/plot/densesig.py
@@ -13,8 +13,6 @@ def dense_sig_fig(cfg, stages, thr_sig=0, label="", thr_udense=1.0):
     for stage in stages:
         df, _ = load(cfg, "evaluate", stage)
         df = df.query("kind != 'remove'")
-        # fp = np.load(f"{path}/../../000footprint.npy")
-        # df["intensity"] = df.signal * df.firmness
         df.loc[df.udense > thr_udense, "kind"] = "background"
         df["col"] = stage
         df["size"] = 1
@@ -39,6 +37,49 @@ def dense_sig_fig(cfg, stages, thr_sig=0, label="", thr_udense=1.0):
         fig.update_annotations(
             selector=dict(text=f"col={stage}"), y=0.8, text=f"{label}{stage}; num={n}"
         )
+    fig.update_layout(
+        template="none",
+        font_size=11,
+        showlegend=False,
+        width=600,
+        height=150,
+        margin=dict(l=35, r=10, t=20, b=32),
+    )
+    return fig
+
+
+def dense_sig_multi_fig(paths, stages, thr_sig=0, thr_udense=1.0):
+    num = []
+    dfs = []
+    for name, path in paths.items():
+        for stage in stages:
+            if stage == 0:
+                pathx = f"{path}/../../{stage:03d}stats.csv"
+            else:
+                pathx = f"{path}/{stage:03d}stats.csv"
+            df = pd.read_csv(pathx)
+            df = df.query("kind != 'remove'")
+            df.loc[df.udense > thr_udense, "kind"] = "background"
+            df["stage"] = stage
+            df["name"] = name
+            df["size"] = 1
+            df["symbol"] = 1
+            dfs.append(df)
+            num.append(df.query("kind == 'cell'").shape[0])
+    df = pd.concat(dfs, axis=0)
+    fig = px.scatter(
+        df,
+        x="udense",
+        y="signal",
+        color="kind",
+        size="size",
+        size_max=3,
+        opacity=0.5,
+        facet_col="stage",
+        facet_row="name",
+    )
+    #for i in range(len(fig.data)):
+    #    fig.data[i].marker.line.width = 0
     fig.update_layout(
         template="none",
         font_size=11,

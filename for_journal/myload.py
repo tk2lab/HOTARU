55fbf1df856862b92a51701b60854a7950d7c597
TAKEKAWA Takashi <takekawa@tk2lab.org>
2023-09-08 23:08:44 +0900

    Add missing file

---
+++ for_journal/myload.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import h5py
+
+
+def load(path, stage, only_df=False):
+    if stage == 0:
+        apath = f"{path}/../../../.."
+        upath = f"{path}/../.."
+    else:
+        apath = path
+        upath = path
+    if only_df:
+        return pd.read_csv(f"{upath}/{stage:03d}stats.csv", index_col=0)
+    with h5py.File(f"{apath}/{stage:03d}footprints.h5") as h5:
+        a = h5["data"][...]
+    u = np.load(f"{upath}/{stage:03d}spike.npy")
+    df = pd.read_csv(f"{upath}/{stage:03d}stats.csv", index_col=0)
+    return a, u, df

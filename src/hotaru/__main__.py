535ead67720ca8124f7a485199651ac1cd1fa825
TAKEKAWA Takashi <takekawa@tk2lab.org>
2026-01-21 02:06:33 +0900

    tune hydra usage

--- src/hotaru/__main__.py
+++ src/hotaru/__main__.py
@@ -1,17 +1,11 @@
-import webbrowser
-from pathlib import Path
-from importlib.resources import read_text
-
 import hydra
 
-from .cui import (
-    run,
-    test,
-)
+from .cui import run
+from .cui import test
 
 
-@hydra.main(version_base=None, config_path="pkg://hotaru.conf", config_name="config")
-def _main(cfg):
+@hydra.main(version_base=None, config_path=None, config_name="config")
+def main(cfg):
     match cfg.mode:
         case "test":
             test(cfg)
@@ -19,16 +13,6 @@ def _main(cfg):
             run(cfg)
         case _:
             raise ValueError()
-
-
-def main():
-    cfg_path = Path("hotaru/default.yaml")
-    if not cfg_path.exists():
-        cfg_path.parent.mkdir(parents=True, exist_ok=True)
-        cfg = read_text("hotaru.conf", "user_sample.yaml")
-        cfg_path.write_text(cfg)
-
-    _main()
 
 
 if __name__ == "__main__":

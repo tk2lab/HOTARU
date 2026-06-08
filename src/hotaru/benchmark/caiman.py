a4ff86748c57ba7709b47c03ba2aacdf0933ab3d
TAKEKAWA Takashi <takekawa@tk2lab.org>
2026-06-08 12:36:22 +0900

    caiman

--- src/hotaru/benchmark/caiman.py
+++ src/hotaru/benchmark/caiman.py
@@ -9,15 +9,20 @@ environ['VECLIB_MAXIMUM_THREADS'] = '1'
 import caiman
 import hydra
 import numpy as np
+import polars as pl
 from caiman.source_extraction.cnmf import CNMF
 from caiman.source_extraction.cnmf.cnmf import load_CNMF
 from caiman.source_extraction.cnmf.params import CNMFParams
 from hydra.utils import get_original_cwd
+from keras.saving import load_model
 from omegaconf import OmegaConf
 from tqdm import trange
 
 from ..saving import auto_save_config
 from ..saving import make_link
+from ..spatial import Footprints  # noqa
+from ..temporal import Traces  # noqa
+from .evaluation import evaluate
 
 
 @hydra.main(version_base=None, config_path='.', config_name='caiman')
@@ -25,13 +30,20 @@ def main(cfg):
     OmegaConf.resolve(cfg)
     cwd = Path().absolute()
     path = (Path(get_original_cwd()) / cfg.path).relative_to(cwd, walk_up=True)
+    data_path = path / cfg.simdata
 
-    imgs_path = path / cfg.simdata / 'imgs'
+    imgs_path = data_path / 'imgs'
     fname_mmap = caiman.save_memmap([str(imgs_path / 'imgs.npy')], order='C')
     yr, (h, w), nt = caiman.load_memmap(fname_mmap)
     images = np.reshape(yr.T, (nt, h, w), order='F')
     hz = float(json.loads((imgs_path / 'config.json').read_text()).get('hz'))
     OmegaConf.update(cfg.params.data, 'fr', hz, force_add=True)
+
+    gt = (
+        load_model(data_path / 'cell' / 'fps.keras').segs.numpy(),
+        load_model(data_path / 'cell' / 'trs.keras').obs.numpy(),
+        pl.read_csv(data_path / 'cell' / 'stats.csv'),
+    )
 
     _c, dview, n_processes = caiman.cluster.setup_cluster(**cfg.cluster)
     cluster = {'dview': dview, 'n_processes': n_processes}
@@ -40,8 +52,22 @@ def main(cfg):
     kwargs = {'imgs_path': imgs_path, 'params': OmegaConf.to_container(cfg.params)}
     for i in trange(cfg.num_refit, ncols=150, desc='caiman fit'):
         step_path = caiman_fit(caiman_path, images, cluster, **kwargs)
+        model = load_CNMF(step_path / 'cnmfe.hdf5', **cluster)
+        est = model.estimates
+        est.evaluate_components(images, model.params, dview=cluster['dview'])
+        evaluate_caiman(est, *gt).write_csv(step_path / 'eval.csv')
         make_link(cwd, f'step{i:03d}', step_path)
         kwargs = {'prev_path': step_path}
+
+
+def evaluate_caiman(est, fps_gt, trs_gt, stats):
+    _, h, w = fps_gt.shape
+    fps = est.A.toarray().reshape(w, h, -1).transpose(2, 1, 0)
+    trs = est.C.copy()
+    df = evaluate(fps, trs, fps_gt, trs_gt)
+    df = df.with_columns(rval=est.r_values, snr=est.SNR_comp)
+    df = pl.concat((df, stats[df['gt'].to_numpy()]), how='horizontal')
+    return df
 
 
 @auto_save_config(0.3, exclude=('images', 'cluster'))
@@ -56,19 +82,6 @@ def caiman_fit(path, images, cluster, **kwargs):
         case _:
             raise ValueError(f'{kwargs}')
     cnmf_model.save(str(path / 'cnmfe.hdf5'))
-
-
-"""
-        re_fps_val = cnmf_refit.estimates.A.toarray().reshape(w, h, -1).transpose(2, 1, 0)
-        np.save(path / f'fps{i}.npy', re_fps_val)
-
-        re_trs0_val = cnmf_refit.estimates.C.copy()
-        np.save(path / f'trs{i}_0.npy', re_trs0_val)
-
-        cnmf_refit.estimates.detrend_df_f(**detrend)
-        re_trs1_val = cnmf_refit.estimates.F_dff.copy()
-        np.save(path / f'trs{i}_1.npy', re_trs1_val)
-"""
 
 
 if __name__ == '__main__':
---
+++ src/hotaru/benchmark/evaluation.py
@@ -0,0 +1,56 @@
+from math import nan
+
+import numpy as np
+import polars as pl
+from scipy.stats import zscore
+
+
+def evaluate(fps, trs, fps_true, trs_true, stats_true=None):
+    nk, h, w = fps.shape
+    ng, nt = trs_true.shape
+    nx = h * w
+
+    fpt_z = zscore(fps_true.reshape(ng, nx), axis=1)
+    trt_z = zscore(trs_true, axis=1)
+    fps_z = zscore(fps.reshape(nk, nx), axis=1)
+    trs_z = zscore(trs, axis=1)
+
+    fps_cor = fps_z @ fpt_z.T / nx
+    trs_cor = trs_z @ trt_z.T / nt
+    cor = fps_cor * trs_cor
+    i, j = greedy_matching(cor)
+
+    match_gt = np.full(nk, -1, dtype='int32')
+    match_gt[i] = j
+
+    fp_score = np.full(nk, nan, dtype='float32')
+    fp_score[i] = fps_cor[i, j]
+
+    tr_score = np.full(nk, nan, dtype='float32')
+    tr_score[i] = trs_cor[i, j]
+
+    df = pl.DataFrame({'gt': match_gt, 'fp_score': fp_score, 'tr_score': tr_score})
+    if stats_true is not None:
+        df = pl.concat((df, stats_true[j]), how='horizontal')
+    return df
+
+
+def greedy_matching(mat):
+    num_results, num_gts = mat.shape
+    max_possible_pairs = min(num_results, num_gts)
+    flat_indices = np.flip(np.argsort(mat.ravel()))
+
+    matched_pairs = []
+    used_results = set()
+    used_gts = set()
+    for flat_idx in flat_indices:
+        i, j = flat_idx // num_gts, flat_idx % num_gts
+        if (i in used_results) or (j in used_gts):
+            continue
+
+        matched_pairs.append((i, j))
+        used_results.add(i)
+        used_gts.add(j)
+        if len(matched_pairs) == max_possible_pairs:
+            break
+    return np.array(matched_pairs, 'int32').T

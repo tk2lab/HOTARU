b5c27933de6cbbd034e5992fea56a2fd3e535637
TAKEKAWA Takashi <takekawa@tk2lab.org>
2026-06-08 12:36:56 +0900

    peakmap do not include edge radius case

--- src/hotaru/spatial/peakmap.py
+++ src/hotaru/spatial/peakmap.py
@@ -82,10 +82,11 @@ class PeakMapCalculator(Model):
         gl = gaussian_laplace_2d_multi(imgs, radius, axis=1)
         gl_max = max_pool_3d(gl, 3)
         gl_peak = gl == gl_max
+        gl_peak = gl_peak.at[:, [0, -1], :, :].set(False)
         gl_peak &= mask
         gl = ops.where(gl_peak, gl, -inf)
         gl_reshape = ops.reshape(gl, (-1, h, w))
         idx = ops.argmax(gl_reshape, axis=0)
-        gl_max = ops.take_along_axis(gl_reshape, idx[None, ...], axis=0)[0]
         t, r = idx // nr, ops.mod(idx, nr)
-        return t, r, gl_max
+        gl_peak = ops.take_along_axis(gl_reshape, idx[None, ...], axis=0)[0]
+        return t, r, gl_peak
